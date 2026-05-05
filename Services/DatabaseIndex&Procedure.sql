add8e2270a57893f2cc08621ff48dd82c3dfac6a
HLong <truongvohoanglong@gmail.com>
2026-05-05 22:14:41 +0700

    Synchronize specialization with subject name & Fast student AvgGPA feature
    - Tweak subject names to match with employee's specialization
    - Add procedure to search for student Avg GPA in database level with only Student ID.
    - Add function in Student.cs making use of above procedure and taking only student's ID, reducing lag in UI level by using single parameter function.
    - Change name: IndexingQuery.sql to DatabaseIndex&Procedure.sql - Refine purpose.

---
+++ Services/DatabaseIndex&Procedure.sql
@@ -0,0 +1,20 @@
+﻿-- Creates an index on FullName to make text searching  fast
+CREATE NONCLUSTERED INDEX IX_Student_FullName ON Student(FullName);
+
+-- Creates an index on ClassID to speed up the JOIN operations during class searches
+CREATE NONCLUSTERED INDEX IX_ClassPlacement_ClassID ON ClassPlacement(ClassID);
+
+-- Stored Procedure to get student average grade by IDs
+GO
+CREATE PROCEDURE usp_GetStudentGPA
+    @StudentID VARCHAR(10)
+AS
+BEGIN
+    SET NOCOUNT ON;
+    -- Already calculated by the system trigger.
+    SELECT 
+        CAST(AVG(AverageScore) AS DECIMAL(5, 2)) AS TotalGPA
+    FROM Score
+    WHERE StudentID = @StudentID;
+END
+GO
--- Services/IndexingQuery.sql
+++
@@ -1,5 +0,0 @@
-﻿-- Creates an index on FullName to make text searching  fast
-CREATE NONCLUSTERED INDEX IX_Student_FullName ON Student(FullName);
-
--- Creates an index on ClassID to speed up the JOIN operations during class searches
-CREATE NONCLUSTERED INDEX IX_ClassPlacement_ClassID ON ClassPlacement(ClassID);
